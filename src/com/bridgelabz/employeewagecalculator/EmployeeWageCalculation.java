e2e065780bd997f727966f87f3714d6c8bd951b9
akash6280 <akashsingh6280@gmail.com>
2021-08-23 23:24:20 +0530

    REFACTORED: Solved using switch case

--- src/com/bridgelabz/employeewagecalculator/EmployeeWageCalculation.java
+++ src/com/bridgelabz/employeewagecalculator/EmployeeWageCalculation.java
@@ -1,22 +1,26 @@
 package com.bridgelabz.employeewagecalculator;
 
 public class EmployeeWageCalculation {
-	public static void main(String[] args) {
-		int fullTime=1;
-		int partTime=2;
-		int employeeWagePerHour=20;
+	public static final int FULL_TIME=1;
+	public static final int PART_TIME=2;
+	public static final int EMPLOYEE_WAGE_PER_HOUR=20;
+	public static void main(String args[]) {
+
 		int employeeHour=0;
 		int employeeWage=0;		
-		double empCheck=Math.floor(Math.random()*10)%3;
-		if(empCheck==fullTime)
+		int empCheck=(int)Math.floor(Math.random()*10)%3;
+		switch(empCheck) {
+		case 1:
 			employeeHour=8;
-		else if(empCheck==partTime)
+			break;
+		case 2:
 			employeeHour=4;
-		else 
+			break;
+		default:
 			employeeHour=0;
-		employeeWage=employeeHour*employeeWagePerHour;
+		}
+		employeeWage=employeeHour*EMPLOYEE_WAGE_PER_HOUR;
 		System.out.println("Employee Wage :"+employeeWage);
 
 	}
-
 }
