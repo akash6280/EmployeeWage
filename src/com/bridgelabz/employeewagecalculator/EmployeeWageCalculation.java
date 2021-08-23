f49f11b60a637d8bc63b153d3b2b1677a99a4f11
akash6280 <akashsingh6280@gmail.com>
2021-08-23 23:33:25 +0530

    MODIFIED: Calculate wages till working days<=20 or working hours<=100

--- src/com/bridgelabz/employeewagecalculator/EmployeeWageCalculation.java
+++ src/com/bridgelabz/employeewagecalculator/EmployeeWageCalculation.java
@@ -5,28 +5,31 @@ public class EmployeeWageCalculation {
 	public static final int PART_TIME=2;
 	public static final int EMPLOYEE_WAGE_PER_HOUR=20;
 	public static final int NUMBER_OF_WORKING_DAYS=20;
-	public static void main(String args[]) {
-
+	public static final int MAXIMUM_HOURS_IN_MONTH=100;
+	
+	public static void main(String[] args) {
 		int employeeHour=0;
-		int employeeWage=0;	
 		int totalEmployeeWage=0;
-		for(int day=0;day<NUMBER_OF_WORKING_DAYS;day++) {
-		int empCheck=(int)Math.floor(Math.random()*10)%3;
-		switch(empCheck) {
-		case 1:
-			employeeHour=8;
-			break;
-		case 2:
-			employeeHour=4;
-			break;
-		default:
-			employeeHour=0;
+		int totalWorkingDays=0;
+		int totalEmployeeHours=0;
+		while(totalWorkingDays<NUMBER_OF_WORKING_DAYS && totalEmployeeHours<=MAXIMUM_HOURS_IN_MONTH){
+		    totalWorkingDays++;
+			int empCheck=(int)Math.floor(Math.random()*10)%3;
+		    switch(empCheck) {
+		         case 1:
+			            employeeHour=8;
+			            break;
+		         case 2:
+			            employeeHour=4;
+			            break;
+		         default:
+			            employeeHour=0;
+		     }
+		     totalEmployeeHours+=employeeHour;
+		     System.out.println("Day# :"+totalWorkingDays+" Employee Hour "+ employeeHour);
 		}
-		employeeWage=employeeHour*EMPLOYEE_WAGE_PER_HOUR;
-		totalEmployeeWage+=employeeWage;
-		System.out.println("Employee Wage :"+employeeWage);
-		}
+		totalEmployeeWage=totalEmployeeHours*EMPLOYEE_WAGE_PER_HOUR;
 		System.out.println("Total Employee Wage :"+totalEmployeeWage);
-
 	}
+
 }
