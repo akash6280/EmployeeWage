29481e13b50a8dfc2dd78b7df5bcf4fc739c3c2e
akash6280 <akashsingh6280@gmail.com>
2021-08-23 23:17:00 +0530

    ADDED:Calculating Part time wage

--- src/com/bridgelabz/employeewagecalculator/EmployeeWageCalculation.java
+++ src/com/bridgelabz/employeewagecalculator/EmployeeWageCalculation.java
@@ -2,17 +2,20 @@ package com.bridgelabz.employeewagecalculator;
 
 public class EmployeeWageCalculation {
 	public static void main(String[] args) {
-		 int fullTime=1;
-			int employeeWagePerHour=20;
-			int employeeHour=0;
-			int employeeWage=0;		
-			double empCheck=Math.floor(Math.random()*10)%2;
-			if(empCheck==fullTime)
-				employeeHour=8;
-			else
-				employeeHour=0;
-			employeeWage=employeeHour*employeeWagePerHour;
-			System.out.println("Employee Wage :"+employeeWage);
+		int fullTime=1;
+		int partTime=2;
+		int employeeWagePerHour=20;
+		int employeeHour=0;
+		int employeeWage=0;		
+		double empCheck=Math.floor(Math.random()*10)%3;
+		if(empCheck==fullTime)
+			employeeHour=8;
+		else if(empCheck==partTime)
+			employeeHour=4;
+		else 
+			employeeHour=0;
+		employeeWage=employeeHour*employeeWagePerHour;
+		System.out.println("Employee Wage :"+employeeWage);
 
 	}
 
