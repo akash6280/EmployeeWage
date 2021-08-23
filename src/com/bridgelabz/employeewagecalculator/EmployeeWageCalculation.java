7199432e1fc065fdbd42aa6ae5b56868221a919e
akash6280 <akashsingh6280@gmail.com>
2021-08-23 22:49:39 +0530

    Inital Commit

---
+++ src/com/bridgelabz/employeewagecalculator/EmployeeWageCalculation.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.employeewagecalculator;
+
+public class EmployeeWageCalculation {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Wage Computation");
+	}
+
+}
