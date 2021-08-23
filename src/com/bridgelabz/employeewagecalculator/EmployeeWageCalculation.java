d833866d9850f4dd87e169a510322e003d675b6b
akash6280 <akashsingh6280@gmail.com>
2021-08-23 23:28:40 +0530

    ADDED: Calculate wages for month

--- src/com/bridgelabz/employeewagecalculator/EmployeeWageCalculation.java
+++ src/com/bridgelabz/employeewagecalculator/EmployeeWageCalculation.java
@@ -4,10 +4,13 @@ public class EmployeeWageCalculation {
 	public static final int FULL_TIME=1;
 	public static final int PART_TIME=2;
 	public static final int EMPLOYEE_WAGE_PER_HOUR=20;
+	public static final int NUMBER_OF_WORKING_DAYS=20;
 	public static void main(String args[]) {
 
 		int employeeHour=0;
-		int employeeWage=0;		
+		int employeeWage=0;	
+		int totalEmployeeWage=0;
+		for(int day=0;day<NUMBER_OF_WORKING_DAYS;day++) {
 		int empCheck=(int)Math.floor(Math.random()*10)%3;
 		switch(empCheck) {
 		case 1:
@@ -20,7 +23,10 @@ public class EmployeeWageCalculation {
 			employeeHour=0;
 		}
 		employeeWage=employeeHour*EMPLOYEE_WAGE_PER_HOUR;
+		totalEmployeeWage+=employeeWage;
 		System.out.println("Employee Wage :"+employeeWage);
+		}
+		System.out.println("Total Employee Wage :"+totalEmployeeWage);
 
 	}
 }
