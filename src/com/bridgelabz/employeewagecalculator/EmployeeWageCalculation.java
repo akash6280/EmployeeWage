8825692aa844e8f8e46b5f883d1380408ec374a5
akash6280 <akashsingh6280@gmail.com>
2021-08-28 06:46:56 +0530

    MODIFIED:Calculate wage for multiple company using employee wage class

--- src/com/bridgelabz/employeewagecalculator/EmployeeWageCalculation.java
+++ src/com/bridgelabz/employeewagecalculator/EmployeeWageCalculation.java
@@ -1,52 +1,63 @@
 package com.bridgelabz.employeewagecalculator;
 
 public class EmployeeWageCalculation {
-	public static final int FULL_TIME=1;
-	public static final int PART_TIME=2;
-	private String companyName;
-	private int employeeWagePerHour;
-	private int numberOfWorkDays;
-	private int maxHours;
-	
-	
- EmployeeWageCalculation(String companyName, int employeeWAgePerHour, int numberOfWorkDays, int maxHours) {
-		this.companyName = companyName;
-		this.employeeWagePerHour = employeeWAgePerHour;
-		this.maxHours = maxHours;
-		this.numberOfWorkDays = numberOfWorkDays;
+	public static final int MAX_WORKING_HOURS = 8;
+	public static final int FULL_TIME = 2;
+	public static final int PART_TIME = 1;
+	public static final int ABSENT = 0;
+
+	private int noOfCompany = 0;
+	private CompanyEmployeeWage[] companyEmployeeWageArray;
+
+
+	public EmployeeWageCalculation() {
+		companyEmployeeWageArray = new CompanyEmployeeWage[5];
 	}
-	public  void computeEmployeeWage()
-	{
-		int employeeHour=0;
-		int totalEmployeeWage=0;
-		int totalWorkingDays=0;
-		int totalEmployeeHours=0;
-		while(totalWorkingDays<numberOfWorkDays && totalEmployeeHours<=maxHours){
-		    totalWorkingDays++;
-			int empCheck=(int)Math.floor(Math.random()*10)%3;
-		    switch(empCheck) {
-		         case 1:
-			            employeeHour=8;
-			            break;
-		         case 2:
-			            employeeHour=4;
-			            break;
-		         default:
-			            employeeHour=0;
-		     }
-		     totalEmployeeHours+=employeeHour;
-		     System.out.println("Day# :"+totalWorkingDays+" Employee Hour "+ employeeHour);
+
+	private void addCompanyEmpWage(String companyName, int wagePerHour, int noOfDays, int maxHoursPerMonth) {
+
+		companyEmployeeWageArray[noOfCompany] = new CompanyEmployeeWage(companyName, wagePerHour, noOfDays, maxHoursPerMonth);
+		noOfCompany++;
+	}
+	
+	public void calculateCompanyEmployeeWage() {
+		for(int index=0;index<noOfCompany;index++) {
+			companyEmployeeWageArray[index].setTotalEmpWage(calculateEmployeeWage(companyEmployeeWageArray[index]));
+			System.out.println(companyEmployeeWageArray[index]);
 		}
-		totalEmployeeWage=totalEmployeeHours*employeeWagePerHour;
-		System.out.println("Company name :"+companyName+" \nTotal Employee Wage :"+totalEmployeeWage);
 	}
-	
+
+	public int calculateEmployeeWage(CompanyEmployeeWage companyEmpWage) {
+		int totalHrs = 0, day = 0;
+		while (totalHrs < companyEmpWage.maxHoursPerMonth && day < companyEmpWage.noOfDays) {
+			int hours = 0;
+			double empCheck = Math.floor(Math.random() * 10) % 3;
+			switch ((int) empCheck) {
+			case FULL_TIME:
+				hours = MAX_WORKING_HOURS;
+				break;
+
+			case PART_TIME:
+				hours = MAX_WORKING_HOURS / 2;
+				break;
+
+			case ABSENT:
+				hours = 0;
+			}
+			totalHrs += hours;
+			day++;
+			System.out.println("Day " + day + ": " + hours + " hours");
+		}
+		return totalHrs * companyEmpWage.wagePerHour;
+	}
+
 	public static void main(String[] args) {
-		EmployeeWageCalculation tcs = new EmployeeWageCalculation("TCS", 20,2,10);
-		EmployeeWageCalculation infosys = new EmployeeWageCalculation("INFOSYS", 10,4,20);
-		EmployeeWageCalculation wipro = new EmployeeWageCalculation("WIPRO", 20,2,10);
-		tcs.computeEmployeeWage();
-		infosys.computeEmployeeWage();
-		wipro.computeEmployeeWage();
+
+		EmployeeWageCalculation empWageCalculator = new EmployeeWageCalculation();
+		empWageCalculator.addCompanyEmpWage("TCS", 20, 6, 10);
+		empWageCalculator.addCompanyEmpWage("WIPRO", 10, 5, 20);	
+		empWageCalculator.addCompanyEmpWage("INFOSYS", 10, 5, 20);
+		empWageCalculator.calculateCompanyEmployeeWage();
+
 	}
 }
